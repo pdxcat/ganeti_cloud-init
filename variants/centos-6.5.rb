2722b0e7a16b7bd7c12e45c04c797f349d2afbed
Sage Imel <sage@cat.pdx.edu>
2014-08-18 15:19:24 -0700

    Add centos 6.5 stuff

--- variants/centos-6.5.rb
+++ variants/centos-6.5.rb
@@ -1,4 +1,4 @@
-$config["bootcmd"].push "sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm"
-$config["bootcmd"].push "yum -y install puppet"
-$config["bootcmd"].push "/tmp/copycfg"
-$config["bootcmd"].push "cp /tmp/puppet.conf /etc/puppet/puppet.conf"
+$config["runcmd"].push "sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm"
+$config["runcmd"].push "yum -y install puppet"
+$config["runcmd"].push "/tmp/copycfg"
+$config["runcmd"].push "cp /tmp/puppet.conf /etc/puppet/puppet.conf"
